f064ce55ec34a3c40a38399236b13cb4e9a95ec1
Bartlomiej <bartlomiejblaszczak1997@gmail.com>
2018-06-02 16:09:05 +0200

    text

---
+++ src/main/java/graphics/Text.java
@@ -0,0 +1,142 @@
+package graphics;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.stb.STBTTAlignedQuad;
+import org.lwjgl.stb.STBTTPackContext;
+import org.lwjgl.stb.STBTTPackedchar;
+import org.lwjgl.system.Callback;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+
+import static graphics.Font.util.IOUtil.ioResourceToByteBuffer;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glVertex2f;
+import static org.lwjgl.stb.STBTruetype.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+import static org.lwjgl.system.MemoryUtil.memAllocFloat;
+
+public class Text implements Displayable
+{
+    private int x, y;
+    private Color c;
+    private String txt;
+
+    private static final int BITMAP_W = 512;
+    private static final int BITMAP_H = 512;
+
+    private static final float[] scale = {
+            84.0f,
+            44.0f
+    };
+
+    private static final int[] sf = {
+            0, 1, 2,
+            0, 1, 2
+    };
+
+    private final STBTTAlignedQuad q  = STBTTAlignedQuad.malloc();
+    private final FloatBuffer xb = memAllocFloat(1);
+    private final FloatBuffer      yb = memAllocFloat(1);
+
+    private static int font_tex;
+    private static STBTTPackedchar.Buffer chardata;
+    private int font = 3;
+
+    public Text(int x, int y, String txt,float r, float g, float b)
+    {
+        this.x=x;
+        this.y=y;
+        this.txt=txt;
+        this.c=new Color(r,g,b,1f);
+    }
+
+    @Override
+    public void display()
+    {
+        int sfont = sf[font];
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glColor3f(c.getR(), c.getG(), c.getB());
+        print(x, y, sfont, txt);
+    }
+
+    private static void drawBoxTC(float x0, float y0, float x1, float y1, float s0, float t0, float s1, float t1) {
+        glTexCoord2f(s0, t0);
+        glVertex2f(x0, y0);
+        glTexCoord2f(s1, t0);
+        glVertex2f(x1, y0);
+        glTexCoord2f(s1, t1);
+        glVertex2f(x1, y1);
+        glTexCoord2f(s0, t1);
+        glVertex2f(x0, y1);
+    }
+
+    private void print(float x, float y, int font, String text)
+    {
+        xb.put(0, x);
+        yb.put(0, y);
+
+        chardata.position(font * 128);
+
+        glEnable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, font_tex);
+
+        glBegin(GL_QUADS);
+        for (int i = 0; i < text.length(); i++) {
+            stbtt_GetPackedQuad(chardata, BITMAP_W, BITMAP_H, text.charAt(i), xb, yb, q, font == 0);
+            drawBoxTC(
+                    q.x0(), q.y0(), q.x1(), q.y1(),
+                    q.s0(), q.t0(), q.s1(), q.t1()
+            );
+        }
+        glEnd();
+    }
+
+    public static void load_font(String path)
+    {
+        font_tex = glGenTextures();
+        chardata = STBTTPackedchar.malloc(6 * 128);
+
+        try (STBTTPackContext pc = STBTTPackContext.malloc())
+        {
+            ByteBuffer ttf = ioResourceToByteBuffer(path, 512 * 1024);
+
+            ByteBuffer bitmap = BufferUtils.createByteBuffer(BITMAP_W * BITMAP_H);
+
+            stbtt_PackBegin(pc, bitmap, BITMAP_W, BITMAP_H, 0, 1, NULL);
+            for (int i = 0; i < 2; i++)
+            {
+                int p = (i * 3 + 0) * 128 + 32;
+                chardata.limit(p + 95);
+                chardata.position(p);
+                stbtt_PackSetOversampling(pc, 1, 1);
+                stbtt_PackFontRange(pc, ttf, 0, scale[i], 32, chardata);
+
+                p = (i * 3 + 1) * 128 + 32;
+                chardata.limit(p + 95);
+                chardata.position(p);
+                stbtt_PackSetOversampling(pc, 2, 2);
+                stbtt_PackFontRange(pc, ttf, 0, scale[i], 32, chardata);
+
+                p = (i * 3 + 2) * 128 + 32;
+                chardata.limit(p + 95);
+                chardata.position(p);
+                stbtt_PackSetOversampling(pc, 3, 1);
+                stbtt_PackFontRange(pc, ttf, 0, scale[i], 32, chardata);
+            }
+            chardata.clear();
+            stbtt_PackEnd(pc);
+
+            glBindTexture(GL_TEXTURE_2D, font_tex);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, BITMAP_W, BITMAP_H, 0, GL_ALPHA, GL_UNSIGNED_BYTE, bitmap);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
