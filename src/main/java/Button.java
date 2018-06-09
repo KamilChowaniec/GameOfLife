9489b10db72e7c44c47ed89fcf7b3f18aaad7915
Bartlomiej <bartlomiejblaszczak1997@gmail.com>
2018-06-10 00:02:02 +0200

    button

--- src/main/java/Button.java
+++ src/main/java/Button.java
@@ -1,18 +1,28 @@
 import graphics.Color;
+import graphics.Rectangle;
+import graphics.Text;
+
+import static org.lwjgl.opengl.GL11.glColor3f;
 
 public class Button implements Positionable {
     private int x, y, width, height;
     private String text;
     private Color color;
     private boolean focused;
+    float r,g,b;
+    private Text t;
 
-    public Button(int x, int y, int width, int height, String text, Color color) {
+    public Button(int x, int y, int width, int height, String text,float r, float g, float b) {
         focused = false;
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.text = text;
+        this.r=r;
+        this.g=g;
+        this.b=b;
+        t=new Text(x+60,y+33,text, 1.0f,1f,1);
     }
 
     public boolean isFocused(int mx, int my) {
@@ -41,6 +51,11 @@ public class Button implements Positionable {
         this.y = y;
     }
 
-
+    public void display()
+    {
+        glColor3f(r, g, b);
+        Rectangle.display(x,y,width,height,true);
+        t.display();
+    }
 
 }
