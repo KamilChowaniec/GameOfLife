f1778c523b732d22bc8ede19d4e67781a4f50863
Aedi <astra97@poczta.fm>
2018-06-26 17:41:19 +0200

    Button

--- src/main/java/Button.java
+++ src/main/java/Button.java
@@ -4,8 +4,7 @@ import graphics.Text;
 
 import static org.lwjgl.opengl.GL11.glColor3f;
 
-public class Button implements Positionable
-{
+public class Button implements Positionable {
     private int x, y, width, height;
     private String text;
     private Color color;
@@ -13,27 +12,31 @@ public class Button implements Positionable
     private Text t;
     private ButtonHandler handler;
 
-    public Button(int x, int y, int width, int height, String text,ButtonHandler handler,float r, float g, float b) {
-        color = new Color(r,g,b,1);
-        this.handler=handler;
+    public Button(int x, int y, int width, int height, String text, ButtonHandler handler, float r, float g, float b) {
+        color = new Color(r, g, b, 1);
+        this.handler = handler;
         focused = false;
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.text = text;
-        t=new Text(x,y+height/2 + (int)Text.getFontHeight()/2,text, 1.0f,1f,1);
+        t = new Text(x, y + height / 2 + (int) Text.getFontHeight() / 2, text, 1.0f, 1f, 1);
     }
 
     public boolean isFocused(int mx, int my) {
         return focused = (mx > x && mx < x + width && my > y && my < y + height);
     }
 
+    public void setHandler(ButtonHandler handler) {
+        this.handler = handler;
+    }
+
     public boolean isFocused() {
         return focused;
     }
 
-    public void press(){
+    public void press() {
         handler.invoke();
     }
 
@@ -59,12 +62,13 @@ public class Button implements Positionable
     public void setPosition(int x, int y) {
         this.x = x;
         this.y = y;
+        t.setX(x);
+        t.setY(y + height / 2 + (int) Text.getFontHeight() / 2);
     }
 
-    public void display()
-    {
+    public void display() {
         glColor3f(color.getR(), color.getG(), color.getB());
-        Rectangle.display(x,y,width,height,true);
+        Rectangle.display(x, y, width, height, true);
         t.display();
     }
 
